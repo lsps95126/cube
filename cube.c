aa7cc749b158bdd9b219ab943f5cc757a78b2ed8
lsps95126 <lsps95126@outlook.com>
2022-11-02 00:40:51 +0800

    Modify Size

--- cube.c
+++ cube.c
@@ -6,9 +6,9 @@
 float A, B, C;
 
 float cubeWidth = 20;
-int width = 160, height = 44;
-float zBuffer[160 * 44];
-char buffer[160 * 44];
+int width = 80, height = 22;
+float zBuffer[80 * 22];
+char buffer[80 * 22];
 int backgroundASCIICode = '.';
 int distanceFromCam = 100;
 float horizontalOffset;
@@ -60,22 +60,22 @@ int main() {
   while (1) {
     memset(buffer, backgroundASCIICode, width * height);
     memset(zBuffer, 0, width * height * 4);
-    cubeWidth = 20;
-    horizontalOffset = -2 * cubeWidth;
-    // first cube
-    for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
-      for (float cubeY = -cubeWidth; cubeY < cubeWidth;
-           cubeY += incrementSpeed) {
-        calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
-        calculateForSurface(cubeWidth, cubeY, cubeX, '$');
-        calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
-        calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
-        calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
-        calculateForSurface(cubeX, cubeWidth, cubeY, '+');
-      }
-    }
+    //cubeWidth = 20;
+    //horizontalOffset = -2 * cubeWidth;
+    //// first cube
+    //for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+    //  for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+    //       cubeY += incrementSpeed) {
+    //    calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+    //    calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+    //    calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+    //    calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+    //    calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+    //    calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+    //  }
+    //}
     cubeWidth = 10;
-    horizontalOffset = 1 * cubeWidth;
+    horizontalOffset = -2 * cubeWidth;
     // second cube
     for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
       for (float cubeY = -cubeWidth; cubeY < cubeWidth;
@@ -89,7 +89,7 @@ int main() {
       }
     }
     cubeWidth = 5;
-    horizontalOffset = 8 * cubeWidth;
+    horizontalOffset = 3 * cubeWidth;
     // third cube
     for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
       for (float cubeY = -cubeWidth; cubeY < cubeWidth;
